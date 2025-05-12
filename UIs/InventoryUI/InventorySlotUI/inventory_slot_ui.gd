1acb0d1e968a74e2a00110a07a337d27a456cf09
KbraDev <ltovar.dev@gmail.com>
2025-05-12 14:14:34 -0600

    sistema de inventario terminado

--- UIs/InventoryUI/InventorySlotUI/inventory_slot_ui.gd
+++ UIs/InventoryUI/InventorySlotUI/inventory_slot_ui.gd
@@ -1,5 +1,23 @@
-extends Panel ## InventorySlotUI
+extends Panel
 
-func set_item(icon_texture: Texture2D, amount: int):
-	$icon.texture = icon_texture
+var item_data: ItemData = null
+
+signal hover_started(item_data, global_position)
+signal hover_ended()
+
+func set_item(item: ItemData, amount: int):
+	item_data = item
+	$icon.texture = item.icon if item != null else null
 	$amount.text = str(amount) if amount > 0 else ""
+	
+
+func _ready():
+	connect("mouse_entered", Callable(self, "_on_mouse_entered"))
+	connect("mouse_exited", Callable(self, "_on_mouse_exited"))
+
+func _on_mouse_entered():
+	if item_data:
+		emit_signal("hover_started", item_data, get_global_position())
+
+func _on_mouse_exited():
+	emit_signal("hover_ended")
--- UIs/InventoryUI/inventory_ui.gd
+++ UIs/InventoryUI/inventory_ui.gd
@@ -1,12 +1,19 @@
-extends Control
+extends Control  ## INVENTARIO
 
 @export var player: Node
 var slots: Array = []
 var inventory_cooldown := false
 
+@onready var tooltip = $ItemTooltip/Label
+
 func _ready() -> void:
-	visible = false  # ← oculta el inventario al empezar
+	visible = false
 	slots = $NinePatchRect/GridContainer.get_children()
+
+	for slot in slots:
+		slot.connect("hover_started", Callable(self, "_on_slot_hover_started"))
+		slot.connect("hover_ended", Callable(self, "_on_slot_hover_ended"))
+
  
 
 func _unhandled_input(event):
@@ -21,12 +28,14 @@ func _unhandled_input(event):
 func toggle_inventory():
 	visible = !visible
 	if visible:
+		player.can_attack = false
 		player.can_move = false
 		player.velocity = Vector2.ZERO  # Asegura que no siga deslizando
 		player.animation.play("idle_" + player.last_direction)  # ← Forzar idle
 		update_ui(player.inventory)
 	else:
 		player.can_move = true
+		player.can_attack = true
 
 
 
@@ -40,6 +49,23 @@ func update_ui(inventory_data: Array):
 		var slot = slots[i]
 		var data = inventory_data[i]
 		if data != null:
-			slot.set_item(data["item_data"].icon, data["amount"])
+			slot.set_item(data["item_data"], data["amount"])
 		else:
 			slot.set_item(null, 0)
+
+
+func _on_slot_hover_started(item_data: ItemData, global_pos: Vector2):
+	if item_data == null:
+		return
+
+	tooltip.text = "Nombre: %s\nDescripción: %s" % [
+		item_data.item_name,
+		item_data.description
+	]
+
+	$ItemTooltip.visible = true
+
+
+
+func _on_slot_hover_ended():
+	$ItemTooltip.visible = false
