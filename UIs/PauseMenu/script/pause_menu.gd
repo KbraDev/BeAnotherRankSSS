b626edd1e9a7578c81940b36fe41b3615f39de18
KbraDev <ltovar.dev@gmail.com>
2025-06-10 16:27:18 -0600

    multi saves slots

--- UIs/PauseMenu/script/pause_menu.gd
+++ UIs/PauseMenu/script/pause_menu.gd
@@ -19,17 +19,26 @@ func toggle_pause():
 		visible = true
 		get_tree().paused = true
 
-
-func _on_save_pressed() -> void:
-	var player = get_tree().get_current_scene().get_node("player")  # O ajustá al path real
-	if player:
-		SaveManager.save_game(player, 1)
-	else:
-		print("❌ No se encontró el nodo del jugador.")
-
-
 func _on_salir_pressed():
 	print("Salir al título (no implementado)")
 
 func _on_close_pressed():
 	toggle_pause()
+
+
+func _on_save1_pressed() -> void:
+	save_on_slot(1)
+
+func _on_save_2_pressed() -> void:
+	save_on_slot(2)
+
+
+func _on_save_3_pressed() -> void:
+	save_on_slot(3)
+
+func save_on_slot(slot: int):
+	var player = get_tree().get_current_scene().get_node("player")
+	if player:
+		SaveManager.save_game(player, slot)
+	else:
+		print("❌ No se encontró el nodo del jugador.")
