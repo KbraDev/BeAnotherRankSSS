dd5de105faa8f6236ad394c2214a7a1110216ef3
kbradev <ltovar.dev@gmail.com>
2025-11-20 13:44:06 -0600

    reworked temple olid

---
+++ items/_Chests/SilverChest/silver_chest.gd
@@ -0,0 +1,86 @@
+extends StaticBody2D
+
+@export var chest_id: String = "" # variable asignable unica para cada cofre
+@export var hits_to_open: int = 3
+var hit_count: int = 0
+var is_open: bool = false
+
+@onready var sfx_hit = $AudioStreamPlayer2D
+
+# Loot table: cada entrada define un ítem, cantidad mínima/máxima y chance
+@export var loot_table: Array[Dictionary] = [
+	{"id": "BronzeCoin", "min": 3, "max": 8, "chance": 1.0},
+	{"id": "SilverCoin", "min": 1, "max": 3, "chance": 0.2},
+	{"id": "GoldCoin", "min": 1, "max": 1, "chance": 0.05},
+	{"id": "SmallHealthPotion", "min": 1, "max": 2, "chance": 1.0}
+]
+
+func _ready():
+	refresh_state()
+
+
+func hit():
+	if is_open:
+		return
+	hit_count += 1
+	sfx_hit.play()
+
+	if hit_count >= hits_to_open:
+		open_chest()
+
+func open_chest():
+	is_open = true
+	$AnimatedSprite2D.play("open")
+	drop_items()
+	
+	# Guardad la KEY del cofre abierto
+	if chest_id != "":
+		ChestRegistry.set_opened(chest_id)
+
+func drop_items():
+	var rng = RandomNumberGenerator.new()
+
+	for entry in loot_table:
+		var chance: float = entry.get("chance", 1.0)
+		if rng.randf() <= chance:
+			var min_amount: int = entry.get("min", 1)
+			var max_amount: int = entry.get("max", 1)
+			var amount: int = rng.randi_range(min_amount, max_amount)
+			spawn_item(entry["id"], amount)
+
+func spawn_item(item_name: String, amount: int = 1):
+	var item_data = ItemDataBase.get_item_by_id(item_name)
+	if not item_data:
+		print("⚠️ Item no encontrado en base:", item_name)
+		return
+
+	var pickup_scene = preload("res://scenes/World_pick-Ups/pick_ups_items.tscn")
+
+	for i in range(amount):
+		var pickup = pickup_scene.instantiate()
+		pickup.item_data = item_data
+		pickup.amount = 1  # cada pickup es 1 ítem
+
+		get_parent().add_child(pickup)
+
+		# Spawn cerca del cofre con variación
+		var offset = Vector2(randf_range(-12, 12), randf_range(-12, 12))
+		pickup.global_position = global_position + offset
+
+		# 👇 Diferenciar entre ItemData y CoinData
+		var name_to_print = ""
+		if item_data is ItemData:
+			name_to_print = item_data.item_name
+		elif item_data is CoinData:
+			name_to_print = item_data.coin_name
+
+		print("✔️ Cofre dropeó:", name_to_print, " en ", pickup.global_position, " offset=", offset)
+		
+
+func refresh_state():
+	if chest_id != "" and ChestRegistry.is_opened(chest_id):
+		is_open = true
+		$AnimatedSprite2D.play("open")
+	else:
+		is_open = false
+		$AnimatedSprite2D.play("close")
